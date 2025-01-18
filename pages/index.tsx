98f2dce5b9efcd36eb674d2c9aa4364d1bdfe3d3
jeremy collyer <collyerdesign@gmail.com>
2025-01-18 08:00:26 -0800

    deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ export default function Home() {
       const oAuthCallback = await res.json();
       window.open(oAuthCallback.url, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
     });
-
     console.log('connect')
   }
 
