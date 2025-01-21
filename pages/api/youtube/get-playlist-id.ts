a875a600167a57139fc9c00ab8e6411768c52cc6
jeremy collyer <collyerdesign@gmail.com>
2025-01-21 15:48:30 -0800

    get YT Shorts playlist ID, and show videos

--- pages/api/youtube/get-playlist-id.ts
+++ pages/api/youtube/get-playlist-id.ts
@@ -1,23 +1,33 @@
 const Youtube = require("youtube-api");
 import { oauth } from "./connect-yt";
 import { Request, Response } from "express";
-import { getTokensCookie }from "@/app/utils";
+import { getTokensCookie } from "@/app/utils";
 
-
-export default async function handler(req:Request, res:Response) {
+export default async function handler(req: Request, res: Response) {
   const { cookie } = req.headers;
   const jsonTokens = getTokensCookie(cookie);
 
   oauth.setCredentials(jsonTokens);
 
-  const userPlaylists = await Youtube.playlists.list({
-    part: ["contentDetails"],
-    mine: true,
-  }).then((response: { data: { items: any[] } }) => response.data.items);
-  
-  const playlistId = userPlaylists[0].id;
+  const userPlaylists = await Youtube.channels
+    .list({
+      part: ["contentDetails"],
+      mine: true,
+    })
+    .then(
+      (response: {
+        data: {
+          items: [
+            { contentDetails: { relatedPlaylists: { uploads: string } } }
+          ];
+        };
+      }) => response.data.items
+    );
+
+  // Get the YT Shorts playlistId
+  const playlistId = userPlaylists[0].contentDetails?.relatedPlaylists?.uploads;
 
   // Set the playlistId in a cookie
   res.setHeader("Set-Cookie", `userPlaylistId=${playlistId}; Path=/`);
-  res.status(200).json({playlistId});
+  res.status(200).json({ playlistId });
 }
