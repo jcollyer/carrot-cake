5770ad968d432b6a47c207f0e77166fcb9439e87
jeremy collyer <collyerdesign@gmail.com>
2025-02-18 06:49:50 -0800

    deploy

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -5,7 +5,7 @@ export const getTokensCookie = (cookie?: string) => {
   
   console.log("cookie-----", cookie);
   console.log("jsTokenCookie------", jsTokenCookie);
-  console.log("decodeURIComponent-------", decodeURIComponent(jsTokenCookie || "[]").split("tokens=")[1]);
+  console.log("decodeURIComponent------", decodeURIComponent(jsTokenCookie || "[]").split("tokens=")[1]);
 
   return JSON.parse(
     decodeURIComponent(jsTokenCookie || "[]").split("tokens=")[1]
