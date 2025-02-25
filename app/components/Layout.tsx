dca85131cd444266f4b6a810aabd3859b8b9bbd5
jeremy collyer <collyerdesign@gmail.com>
2025-02-25 10:49:45 -0800

    add google metadata

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,6 +1,14 @@
 import Navbar from '@/app/components/Navbar';
 import Footer from '@/app/components/Footer';
 
+export const metadata = {
+  title:  "Carrot Cake",
+  description: "Youtube Upload & Schedule App",
+  verification: {
+    google: "google-site-verification=ivuLchF5i_upo7_HQSD4VtFxv0fEcT52mvYsBT7tSLQ",
+  },
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-screen bg-gray-50">
