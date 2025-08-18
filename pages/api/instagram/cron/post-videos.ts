634ddd57a5900e7add12ebc07cfd17116c9133d4
jeremy collyer <collyerdesign@gmail.com>
2025-08-18 13:47:36 -0700

    post videos endpoint export defautl

--- pages/api/instagram/cron/post-videos.ts
+++ pages/api/instagram/cron/post-videos.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(req: Request) {
+export default async function GET(req: Request) {
   if (
     req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
   ) {
